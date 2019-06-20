d9a148f0d7c61fc7c2e93ef286914cf33abcf94c
Melissa Real <melissa.real@hotmail.com>
2019-06-20 18:54:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,23 +7,25 @@ function getToken() {
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  fetch(`https://api.github.com/repos/${repo}/forks`, {
+  const repo = 'https://api.github.com/learn-co-curriculum/js-ajax-fetch-lab/forks';
+  //use fetch to fork it!
+  fetch(repo, {
     method: 'POST',
     headers: {
       Authorization: `token ${getToken()}`
     }
   })
-    .then(response => response.json())
-    .then(json => showResults(json));
+  .then(response => response.json())
+  .then(reply =>  showResults(reply));
+
 }
 
 function showResults(json) {
+  //use this function to display the results from forking via the API
   document.getElementById('results').innerHTML = `<a href=${json.html_url}>${
-    json.html_url
-  }</a>`;
+     json.html_url
+   }</a>`;
 }
-
 
 function createIssue() {
   //use this function to create an issue based on the values input in index.html
